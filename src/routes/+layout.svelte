b9c4e00527169068069c492092e6769b60900a53
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-06-23 19:38:11 +0200

    feat(layout): restructure layout to include title and navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import Nav from '$lib/components/nav.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<title>AuthHub</title>
+
+<div class="bg-base-300 flex h-fit min-h-screen flex-col">
+	<Nav />
+	{@render children()}
+</div>
