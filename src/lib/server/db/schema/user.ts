e2f39b7d3336b6f814f4cc8a73527f0861b52520
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-06-24 17:39:44 +0200

    feat(db): update user schema by adding email, isAdmin, createdAt, updatedAt, and lastLogin fields

--- src/lib/server/db/schema/user.ts
+++ src/lib/server/db/schema/user.ts
@@ -2,9 +2,13 @@ import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
 	id: text('id').primaryKey(),
-	age: integer('age'),
+	email: text('email').unique(),
 	username: text('username').notNull().unique(),
-	passwordHash: text('password_hash').notNull()
+	passwordHash: text('password_hash').notNull(),
+	isAdmin: integer('is_admin', { mode: 'boolean' }).default(false).notNull(),
+	createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
+	updatedAt: integer('updated_at', { mode: 'timestamp' }).notNull(),
+	lastLogin: integer('last_login', { mode: 'timestamp' })
 });
 
 export type User = typeof user.$inferSelect;
