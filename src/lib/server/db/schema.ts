2ae5b8c065940c067670ec85d52764a6ad78e9df
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-06-24 18:00:14 +0200

    feat(db): include apiToken in schema for token management

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,7 @@
 import { user, type User } from './schema/user';
 import { session, type Session } from './schema/session';
+import { apiToken, type ApiToken } from './schema/token';
 
-export const schema = { user, session };
+export const schema = { user, session, token: apiToken };
 
-export { user, type User, session, type Session };
+export { user, type User, session, type Session, apiToken as token, type ApiToken as Token };
