bd164d3f82d8854c2afca2dd8c5a5db4e0c1394e
Black Tiger <66371975+BlackTiger007@users.noreply.github.com>
2025-06-29 09:56:08 +0200

    feat(error): add error handling page for displaying error messages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<div class="flex h-full items-center justify-center">
+	<div class="hero">
+		<div class="hero-content text-center">
+			<div class="max-w-md">
+				<h1 class="mb-5 text-5xl font-bold opacity-10 lg:text-7xl xl:text-9xl">{page.status}</h1>
+				<p class="mb-5">{page.error?.message}</p>
+				<div class="flex justify-center gap-4">
+					<a class="btn btn-outline" href="/">Zurück</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
